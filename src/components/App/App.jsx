be198a6be7f5aba22e027cf420120546eea31e9e
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-06-04 15:57:12 +0300

    main : Added Edu

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import styles from './App.module.css';
 import Skills from './Skills/Skills';
+import Edu from './Edu/Edu';
 
 function App()
 {
     return (
         <div className={styles.wrapper}>
+            <Edu />
             <Skills />
         </div>
     );
---
+++ src/components/App/Edu/Edu.jsx
@@ -0,0 +1,29 @@
+function Edu()
+{
+    return (
+        <div>
+            <h3>Education</h3>
+            <ul>
+                <li>
+                    2012-2016 - Art secondary school #10 of the city of Brest
+                    <p>I studied in a specialized art class.</p>
+                </li>
+                <li>
+                    2016-2017 - secondary school #28 of the city of Brest
+                    <p>I studied in a specialized physics and mathematics class.</p>
+                </li>
+                <li>
+                    2017-2019 - Lyceum #1 named after A. S. Pushkin
+                    <p>I studied in a specialized information technology class.</p>
+                </li>
+                <li>
+                    2019-2024 - Brest State Technical University
+                    <p>I am studying for the specialty "software engineer"
+                        in the group "software" (PO-4).</p>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Edu;
